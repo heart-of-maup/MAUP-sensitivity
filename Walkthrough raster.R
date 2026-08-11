cae9c1883933970169f6785eeb93590c6e9098f5
heart-of-maup <yexiangmaup@outlook.com>
2026-08-12 01:37:10 +0800

    Update Walkthrough raster.R

--- Walkthrough raster.R
+++ Walkthrough raster.R
@@ -1,7 +1,10 @@
 # On the sensitivities to the modifiable areal unit problem
 # Raster walk-through: PRISM precipitation and county zones
 # YE, Xiang 叶翔; CHEN, Jiayi 陈佳怡
-# 2026-07-01
+# 2026-08-12
+
+# Please cite the following reference when part or all of the code in this file is reused under the license of CC-BY-4.0:
+# Ye, X., & Chen, J. (2026). On the sensitivities to the modifiable areal unit problem. Big Earth Data, 1–36. https://doi.org/10.1080/20964471.2026.2692263
 
 # 13. Setup for raster data processing
 library(terra)
